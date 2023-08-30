ef62d5d9412bf12c46230eb8566d926dd8efbb30
ma-si- <Kouma9140@gmail.com>
2023-08-31 07:08:57 +0900

    up

--- Assets/GameOverPlayer.cs
+++ Assets/GameOverPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameOverPlayer : MonoBehaviour
 {
+    Vector3 Playerpos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class GameOverPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Playerpos = this.transform.position;
+        if(Playerpos.y < -5 )
+        {
+            Playerpos.y = 10;
+        }
     }
 }
