25704f78ce3db05ff8d418bd2f1ceafc80f41b02
ma-si- <Kouma9140@gmail.com>
2023-08-30 18:53:04 +0900

    up

--- Assets/CoinContoroller.cs
+++ Assets/CoinContoroller.cs
@@ -15,7 +15,6 @@ public class CoinContoroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(count);
         if(count == 100)
         {
             ScoreManager.score_num += 1000;
--- Assets/TitleButton.cs
+++ Assets/TitleButton.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleButton : MonoBehaviour
 {
+
+    GameObject Fade;
+    bool isFlag;
+    int count;
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+        Fade = GameObject.Find("Panel");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isFlag)
+        {
+            count++;
+        }
+        if (count > 50)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    public void ClickStartButton()
+    {
+        Fade.SetActive(true);
+        Fade.GetComponent<Feedin>().IsFadeOut();
+        isFlag = true;
     }
 }
