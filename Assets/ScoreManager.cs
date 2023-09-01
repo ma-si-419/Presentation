23b4e4f2402cea80a98333a6fc8db5ed19d45b05
ma-si- <Kouma9140@gmail.com>
2023-09-01 12:35:35 +0900

    up

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -8,11 +8,13 @@ public class ScoreManager : MonoBehaviour
 
 
     public GameObject score_object = null;
-    public static int score_num = 0;//スコア変数
-    public static int score_count = 0;
+    public static int score_num;//スコア変数
+    public static int score_count;
     bool isBonusFlag = false;
     void Start()
     {
+        score_num = 0;
+        score_count = 0;
 
     }
 
@@ -24,11 +26,11 @@ public class ScoreManager : MonoBehaviour
         // テキストの表示を入れ替える
         score_text.text = "Score" + score_num;
         //スコアの値を調整する
-        if(score_count == 100 && isBonusFlag == false)
+        if (score_count == 100 && isBonusFlag == false)
         {
             score_num += 5000;
-            isBonusFlag= true;
-        
+            isBonusFlag = true;
+
         }
     }
 }
